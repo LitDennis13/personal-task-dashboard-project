8381971bdb54e3e95bcb023aff2edfe6f1d2da86
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-07-26 06:53:31 -0600

    create selection style when selecting a todo list

--- src/components/todo_list/todo_list.tsx
+++ src/components/todo_list/todo_list.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import styles from "./todo_list.module.css";
 
 function TodoList() {
-    let numOfSideBarOptions = 1;
+    let [sideBarOption, setSideBarOption] = useState(0);
+
     const sideBarOptions = [
         "My Day",
     ];
 
+    function sideBarOptionOnClick(index: number) {
+        setSideBarOption(index);
+    }
+
     function loadSideBarOptions() {
         let options: any[] = [];
+        let id: string;
         sideBarOptions.map((element, index) => {
-            options[index] = <button key={index}>{element}</button>;
+            if (index == sideBarOption) {
+                id = styles.selected;
+            }
+            else {
+                id = "";
+            }
+            options[index] = <button id={id} key={index} onClick={() => sideBarOptionOnClick(index)}>{element}</button>;
         });
 
         return options;
