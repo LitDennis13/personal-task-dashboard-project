1583e60dcf48870965f84d9f69ff79a256c37471
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-07-25 18:13:06 -0600

    setup layout for todo list tab

--- src/components/todo_list/todo_list.tsx
+++ src/components/todo_list/todo_list.tsx
@@ -1,9 +1,27 @@
 import styles from "./todo_list.module.css";
 
 function TodoList() {
+    let numOfSideBarOptions = 1;
+    const sideBarOptions = [
+        "My Day",
+    ];
 
-    return <div>
-        <h1>Todo List</h1>
+    function loadSideBarOptions() {
+        let options: any[] = [];
+        sideBarOptions.map((element, index) => {
+            options[index] = <button key={index}>{element}</button>;
+        });
+
+        return options;
+    }
+
+
+
+    return <div className={styles.mainStyle}>
+        <div className={styles.sideBar}>
+            {loadSideBarOptions()}
+        </div>
+        <div className={styles.mainArea}></div>
     </div>
 }
 
