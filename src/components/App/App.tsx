14f06ee8cc80c2375afd5c06d55bc61196db1858
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-07-27 17:23:16 -0600

    feature: add function to make new lists

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,22 @@ import useTimer from "../custom_hooks/use_timer";
 
 import styles from "./page_layout.module.css";
 
+export interface TodoType {
+    todoID: number;
+    name: string;
+    note: string;
+    hasNote: boolean;
+    isComplete: boolean;
+}
+
+export interface TodoListType {
+    listID: number;
+    name: string;
+    list: TodoType[];
+
+}
+
+
 function setDocumentTitle(title: string) {
     document.title = title;
 }
@@ -16,12 +32,22 @@ function App() {
     let [timerHasStarted, setTimerHasStarted] = useState(false);
     let [option, timerStarted, timeRemaining, optionSet, setTimerStarted] = useTimer(appName, setDocumentTitle, 0);
 
+    // Todo List stuff
+    let defaultTodoListData: TodoListType = {
+        listID: 0,
+        name: "My Day",
+        list: [],
+    };
+    let [newID, setNewID] = useState(1);
+    let [todoListData, setTodoListData] = useState([defaultTodoListData]);
+
     return <div className={styles.mainStyle}>
         <div className={styles.navigationBarLocation}>
             <NavigationBar />
         </div>
         <div className={styles.mainContent}>
-            <Outlet context={[appName, [option, timerStarted, timeRemaining, optionSet, setTimerStarted], [timerHasStarted, setTimerHasStarted], setDocumentTitle]}/>
+            <Outlet context={[appName, [option, timerStarted, timeRemaining, optionSet, setTimerStarted], [timerHasStarted, setTimerHasStarted], setDocumentTitle,
+                [todoListData, setTodoListData], [newID, setNewID]]}/>
         </div>
     </div>
 }
--- src/components/todo_list/todo_list.tsx
+++ src/components/todo_list/todo_list.tsx
@@ -1,38 +1,56 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 import styles from "./todo_list.module.css";
 
+import type { TodoType, TodoListType } from "../App/App";
+
 function TodoList() {
-    let [sideBarOption, setSideBarOption] = useState(0);
+    let [todoListOnDisplayID, setTodoListOnDisplayID] = useState(0);
 
-    const sideBarOptions = [
-        "My Day",
-    ];
+    let [todoListData, setTodoListData] = useOutletContext<any>()[4];
+    let [newID, setNewID] = useOutletContext<any>()[5];
+    
 
-    function sideBarOptionOnClick(index: number) {
-        setSideBarOption(index);
+    function sideBarOptionOnClick(listID: number) {
+        setTodoListOnDisplayID(listID);
     }
 
     function loadSideBarOptions() {
         let options: any[] = [];
         let id: string;
-        sideBarOptions.map((element, index) => {
-            if (index == sideBarOption) {
+        todoListData.map((element: TodoListType, index: number) => {
+            if (element.listID == todoListOnDisplayID) {
                 id = styles.selected;
             }
             else {
                 id = "";
             }
-            options[index] = <button id={id} key={index} onClick={() => sideBarOptionOnClick(index)}>{element}</button>;
+            options[index] = <button id={id} key={index} onClick={() => sideBarOptionOnClick(element.listID)}>{element.name}</button>;
         });
 
         return options;
+    }
+
+    function onNewListClick() {
+        let newTodoList: TodoListType = {
+            listID: newID,
+            name: "Untitled List",
+            list: []
+        };
+
+
+        setTodoListData([...todoListData, newTodoList]);
+        setNewID(newID + 1);
     }
 
 
 
     return <div className={styles.mainStyle}>
         <div className={styles.sideBar}>
-            {loadSideBarOptions()}
+            <div className={styles.listTodoLists}>
+                {loadSideBarOptions()}
+            </div>
+            <button className={styles.addTodoList} onClick={() => onNewListClick()}>+ New List</button>
         </div>
         <div className={styles.mainArea}></div>
     </div>
