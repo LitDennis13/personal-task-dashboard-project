6752a24c5d3082f0c55bd61c989e3e21030cfdb1
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-07-28 14:33:19 -0600

    feature: toto list name gets displayed

--- src/components/todo_list/todo_list.tsx
+++ src/components/todo_list/todo_list.tsx
@@ -5,27 +5,28 @@ import styles from "./todo_list.module.css";
 import type { TodoType, TodoListType } from "../App/App";
 
 function TodoList() {
-    let [todoListOnDisplayID, setTodoListOnDisplayID] = useState(0);
-
     let [todoListData, setTodoListData] = useOutletContext<any>()[4];
     let [newID, setNewID] = useOutletContext<any>()[5];
+
+    let [selectedTodoList, setSelectedTodoList] = useState<TodoListType>(todoListData[0]);
+
     
 
-    function sideBarOptionOnClick(listID: number) {
-        setTodoListOnDisplayID(listID);
+    function sideBarOptionOnClick(todoList: TodoListType) {
+        setSelectedTodoList(todoList);
     }
 
     function loadSideBarOptions() {
         let options: any[] = [];
         let id: string;
         todoListData.map((element: TodoListType, index: number) => {
-            if (element.listID == todoListOnDisplayID) {
+            if (element.listID == selectedTodoList.listID) {
                 id = styles.selected;
             }
             else {
                 id = "";
             }
-            options[index] = <button id={id} key={index} onClick={() => sideBarOptionOnClick(element.listID)}>{element.name}</button>;
+            options[index] = <button id={id} key={index} onClick={() => sideBarOptionOnClick(element)}>{element.name}</button>;
         });
 
         return options;
@@ -40,9 +41,14 @@ function TodoList() {
 
 
         setTodoListData([...todoListData, newTodoList]);
+        setSelectedTodoList(newTodoList);
         setNewID(newID + 1);
     }
 
+    function loadTodoListName() {
+        return selectedTodoList.name;
+
+    }
 
 
     return <div className={styles.mainStyle}>
@@ -52,7 +58,15 @@ function TodoList() {
             </div>
             <button className={styles.addTodoList} onClick={() => onNewListClick()}>+ New List</button>
         </div>
-        <div className={styles.mainArea}></div>
+        <div className={styles.mainArea}>
+            <div className={styles.todoListNameDisplay}>
+                <p>{loadTodoListName()}</p>
+            </div>
+            <div className={styles.todoListDisplay}>{}</div>
+            <form className={styles.addTodoArea}>
+                {/* <input type="text" placeholder="Add Todo" /> */}
+            </form>
+        </div>
     </div>
 }
 
