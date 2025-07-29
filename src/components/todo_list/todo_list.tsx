aa56b570b1e7714c259dab72c8dfda3d4c391a93
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-07-29 01:28:50 -0600

    feature: empty todo list names get reset
    the names are reset to the default
    "Untitled List" name

--- src/components/todo_list/todo_list.tsx
+++ src/components/todo_list/todo_list.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import styles from "./todo_list.module.css";
 
 import type { TodoType, TodoListType } from "../App/App";
+
+function emptyOrWhiteSpace(inString: string) {
+    for (let i = 0; i < inString.length; i++) {
+        if (inString[i] !== " ") {
+            return false;
+        }
+    }
+    return true;
+}
 
 function TodoList() {
     let [todoListData, setTodoListData] = useOutletContext<any>()[4];
     let [newID, setNewID] = useOutletContext<any>()[5];
 
     let [selectedTodoList, setSelectedTodoList] = useState<TodoListType>(todoListData[0]);
-
-    
 
     function sideBarOptionOnClick(todoList: TodoListType) {
         setSelectedTodoList(todoList);
@@ -19,6 +26,7 @@ function TodoList() {
     function loadSideBarOptions() {
         let options: any[] = [];
         let id: string;
+        
         todoListData.map((element: TodoListType, index: number) => {
             if (element.listID == selectedTodoList.listID) {
                 id = styles.selected;
@@ -39,17 +47,42 @@ function TodoList() {
             list: []
         };
 
-
         setTodoListData([...todoListData, newTodoList]);
         setSelectedTodoList(newTodoList);
         setNewID(newID + 1);
-    }
-
-    function loadTodoListName() {
-        return selectedTodoList.name;
 
     }
 
+    function updateTodoListName(event: React.ChangeEvent<HTMLInputElement>) {
+        let name = event.target.value;
+        selectedTodoList.name = name;
+
+        for (let i = 0; i < todoListData.length; i++) {
+            if (todoListData[i].listID == selectedTodoList.listID) {
+                todoListData[i] = selectedTodoList;
+                break;
+            }
+        }
+        setTodoListData([...todoListData]);
+    }
+
+    function loadTodoListNameField() {
+        if (selectedTodoList.listID == 0) {
+            return <input type="text" value={selectedTodoList.name} onChange={(event) => updateTodoListName(event)} readOnly/>;
+        }
+        else {
+            return <input type="text" value={selectedTodoList.name} onChange={(event) => updateTodoListName(event)} onBlur={() => fixEmptyTodoListNames()} />;
+        }
+    }
+
+    function fixEmptyTodoListNames() {
+        for (let i = 0; i < todoListData.length; i++) {
+            if (todoListData[i].listID == selectedTodoList.listID && emptyOrWhiteSpace(todoListData[i].name)) {
+                todoListData[i].name = "Untitled List";
+                setTodoListData([...todoListData]);
+            }
+        }
+    }
 
     return <div className={styles.mainStyle}>
         <div className={styles.sideBar}>
@@ -60,7 +93,7 @@ function TodoList() {
         </div>
         <div className={styles.mainArea}>
             <div className={styles.todoListNameDisplay}>
-                <p>{loadTodoListName()}</p>
+                {loadTodoListNameField()}
             </div>
             <div className={styles.todoListDisplay}>{}</div>
             <form className={styles.addTodoArea}>
