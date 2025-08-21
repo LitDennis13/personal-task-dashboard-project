d343c6b8c88dd58311d94b4fe5c5d681d523ea81
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-08-20 21:33:24 -0600

    add basic note editor

--- src/components/notes/notes.tsx
+++ src/components/notes/notes.tsx
@@ -6,11 +6,18 @@ import AddNoteIcon from "../../assets/images/add_note_icon.svg";
 import type { NoteType } from "../App/App";
 
 function Notes() {
+    const EDIT_NOTE_AREA_DIV_ID = "EditNoteAreaDiv";
+    const EDIT_NOTE_AREA_ID = "EditNoteArea";
+
     let [notesData, setNotesData] = useOutletContext<any>()[6];
     let [newID, setNewID] = useOutletContext<any>()[5];
 
     let mainPage = useRef<HTMLDivElement>(null);
     let [scrollBarPadding, setScrollBarPadding] = useState(false);
+
+    let editNoteDialog = useRef<HTMLDialogElement>(null);
+    let editNoteArea = useRef<HTMLTextAreaElement>(null);
+    let [selectedNoteIndex, setSelectedNoteIndex] = useState(-1);
 
     function checkAndHandleScrollBarLoaded() {
         if (mainPage !== null && mainPage.current instanceof HTMLDivElement) {
@@ -20,6 +27,13 @@ function Notes() {
             else {
                 setScrollBarPadding(false);
             }
+        }
+    }
+
+    function noteOnClick(noteIndex: number) {
+        if (editNoteDialog !== null && editNoteDialog.current instanceof HTMLDialogElement) {
+            editNoteDialog.current.showModal();
+            setSelectedNoteIndex(noteIndex);
         }
     }
 
@@ -42,7 +56,7 @@ function Notes() {
                 }
             }
 
-            let noteEntry = <button key={i} className={styles.noteEntry}>
+            let noteEntry = <button key={i} className={styles.noteEntry} onClick={() => noteOnClick(i)}>
                 <textarea className={styles.title} value={title} readOnly></textarea>
                 <textarea className={styles.note} value={note} readOnly></textarea>
             </button>;
@@ -62,6 +76,28 @@ function Notes() {
         </button>;
     }
 
+    function noteEditorValue() {
+        if (selectedNoteIndex == -1) {
+            return "";
+        }
+        else {
+            return (notesData[selectedNoteIndex] as NoteType).note;
+        }
+    }
+
+    function noteEditorOnChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        (notesData[selectedNoteIndex] as NoteType).note = event.target.value;
+
+        setNotesData([...notesData]);
+    }
+
+    function onDialogClick(event: React.MouseEvent<HTMLDialogElement, MouseEvent>) {
+        if ((event.target as Element).id !== EDIT_NOTE_AREA_ID && (event.target as Element).id !== EDIT_NOTE_AREA_DIV_ID&& editNoteDialog !== null && editNoteDialog.current instanceof HTMLDialogElement) {
+            editNoteDialog.current.close();
+            setSelectedNoteIndex(-1);
+        }
+    }
+
     useEffect(() => {
         checkAndHandleScrollBarLoaded();
     }, []);
@@ -69,6 +105,12 @@ function Notes() {
     return <div ref={mainPage} className={styles.mainPage + " " + (scrollBarPadding ? styles.mainPageWithScrollBar : styles.mainPageNoScrollBar)}>
         {loadNotes()}
         {loadAddNoteButton()}
+
+        <dialog ref={editNoteDialog} className={styles.editNoteDialog} onMouseDown={(event) => onDialogClick(event)}>
+            <div id={EDIT_NOTE_AREA_DIV_ID}>
+                <textarea id={EDIT_NOTE_AREA_ID} ref={editNoteArea} value={noteEditorValue()} onChange={(event) => noteEditorOnChange(event)}></textarea>
+            </div>
+        </dialog>
     </div>
 }
 
