0373073f9cb7705a4e650b9652610ed3791c189f
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-08-27 11:18:03 -0600

    remove parameter on closeTodoEditArea function
    this function is located in the todo_list.tsx file

--- src/components/todo_list/todo_list.tsx
+++ src/components/todo_list/todo_list.tsx
@@ -85,8 +85,8 @@ function TodoList() {
 
     let [selectedTodoID, setSelectedTodoID] = useOutletContext<any>()[8];
 
-    function closeTodoEditArea(instantClose: boolean = false) {
-        if (!instantClose) {
+    function closeTodoEditArea() {
+        if (selectedTodoID !== -1) {
             let todo: TodoType = newTodoDefaultState;
             let todoIndex: number = -1;
             for (let i = 0; i < selectedTodoList.list.length; i++) {
@@ -114,7 +114,7 @@ function TodoList() {
 
     function sideBarOptionOnClick(todoList: TodoListType) {
         setSelectedTodoList(todoList);
-        closeTodoEditArea(true);
+        closeTodoEditArea();
     }
 
     function loadSideBarOptions() {
@@ -417,7 +417,7 @@ function TodoList() {
         }
 
         setTodoListData([...todoListData]);
-        closeTodoEditArea(true);
+        closeTodoEditArea();
         setDeleteTodoPressed(false);
     }
 
@@ -465,7 +465,7 @@ function TodoList() {
                 && (event.target.parentNode as Element).id !== EDIT_TODO_AREA
                 && (event.target as Element).id !== EDIT_TODO_AREA) {
 
-                closeTodoEditArea(true);
+                closeTodoEditArea();
             }
 
             if (event.target.id !== DELETE_LIST_BUTTON) {
