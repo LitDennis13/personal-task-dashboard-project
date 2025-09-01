acbdbd5d7e7f5e5b5474ebde51568fcdbd12621c
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-08-31 23:12:42 -0600

    create project for backend

---
+++ backend/src/main/java/com/antwibuadum/personal_task_dashboard/PersonalTaskDashboardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.antwibuadum.personal_task_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaskDashboardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalTaskDashboardProjectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/antwibuadum/personal_task_dashboard/PersonalTaskDashboardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antwibuadum.personal_task_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskDashboardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
