7d8c537c5b52e518cb8023a373f63bf41922b8fd
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2025-10-06 22:27:10 -0600

    continue developing sql integration with backend

--- backend/src/main/java/com/antwibuadum/personal_task_dashboard/new_id/NewIDService.java
+++ backend/src/main/java/com/antwibuadum/personal_task_dashboard/new_id/NewIDService.java
@@ -2,7 +2,23 @@ package com.antwibuadum.personal_task_dashboard.new_id;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class NewIDService {
     private NewIDRepository newIDRepository;
+
+    public NewIDService(NewIDRepository newIDRepository) {
+        this.newIDRepository = newIDRepository;
+    }
+
+    public NewID getCurrentNewID() {
+        Optional<NewID> value = newIDRepository.findById(0);
+        if (value.isPresent()) {
+            return value.get();
+        }
+        else {
+            return new NewID(-1);
+        }
+    }
 }
